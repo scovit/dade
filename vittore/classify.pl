d045967cae352b935e309910a782af9fce136336
Vittore F. Scolari <vittore.scolari@pasteur.fr>
2015-01-29 15:10:17 +0100

    Update classify program, global overhaul

--- vittore/classify.pl
+++ vittore/classify.pl
@@ -2,133 +2,80 @@
 use strict;
 use warnings;
 
-# This the mapping pipeline (Mirny)
+# Takes as input the mapped reads and output the contact list with flags
 #
 
-if ($#ARGV != 4) {
-	print "usage: ./classify.pl leftmap rightmap rsttable classification genomel\n";
+if ($#ARGV != 2) {
+	print "usage: ./classify.pl leftmap rightmap classification\n";
 	exit;
 };
 
-my ($leftmapfn, $rightmapfn, $rsttablefn, $classificationfn, $genomel) = @ARGV;
+my ($leftmapfn, $rightmapfn, $classificationfn) = @ARGV;
 
-# my $genomel = 4639675;
-my $rstdistmin = 2;
-my $basedistmin = 30;
+my $minqual = 30;
 
-open RSTTABLE, "<", $rsttablefn or die $!;
-my @start; my @stop;
-while (<RSTTABLE>) {
-    chomp;
-    my ($num, $st, $en) = split("\t", $_);
-    push @start, $st;
-    push @stop, $en;
+# open input files
+if ($leftmapfn =~ /\.gz$/) {
+    open(LEFTMAP, "zcat $leftmapfn |");
+} else {
+    open(LEFTMAP, "< $leftmapfn");
 }
-close RSTTABLE;
-
-sub findinrst {
-    my $ele = $_[0];
-
-    my $top = $#start; my $bottom = 0;
-    while (1) {
-        my $index = int(($top + $bottom)/2);
-
-        if ($ele >= $start[$index] && $ele < $stop[$index]) {
-            return $index;
-            last;
-        } elsif ($ele < $start[$index]) {
-            $top = $index - 1;
-        } elsif ($ele >= $stop[$index]) {
-            $bottom = $index + 1;
-        }
-    }
+if ($rightmapfn =~ /\.gz$/) {
+    open(RIGHTMAP, "zcat $rightmapfn |");
+} else {
+    open(RIGHTMAP, "< $rightmapfn");
 }
-
-
-
-open(LEFTMAP, "zcat $leftmapfn |");
-open(RIGHTMAP, "zcat $rightmapfn |");
-open(CLASSIFIC, "| gzip > $classificationfn");
+# open output file
+if  ($classificationfn =~ /\.gz$/) {
+    open(CLASSIFIC, "| gzip > $classificationfn");
+} else {
+    open(CLASSIFIC, "> $classificationfn");
+}
 
 print "Starting classification\n";
 
 use constant {
-    FL_LEFT_NOTFOUND => 2,
-    FL_RIGHT_NOTFOUND => 1,
+    FL_LEFT_ALIGN => 2,
+    FL_RIGHT_ALIGN => 1,
     FL_LEFT_INVERSE => 8,
     FL_RIGHT_INVERSE => 4,
-    FL_INVERSE => 16,
+    FL_INTRA_CHR => 16,
 };
 
 
-my $flag = 0;
-my @left = ();
-my @right = ();
 for (my $num = 0; ; $num++) {
 
-    if (((~$flag) & FL_LEFT_NOTFOUND ) && !eof(LEFTMAP)) {
-	my $ls = <LEFTMAP>;
-	chomp($ls); @left = split("\t", $ls);
-    }
-    if (((~$flag) & FL_RIGHT_NOTFOUND ) && !eof(RIGHTMAP)){
-	my $rs = <RIGHTMAP>;
-	chomp($rs); @right = split("\t", $rs);
-    }
+    my $ls = <LEFTMAP>; chomp($ls);
+    my ($leftnum, $leftnam, $leftflag, $leftchr, $leftpos, $leftqual, $leftasize, $leftrst) = split("\t", $ls);
+    my $rs = <RIGHTMAP>; chomp($rs);
+    my ($rightnum, $rightnam, $rightflag, $rightchr, $rightpos, $rightqual, $rightasize, $rightrst) = split("\t", $rs);
 
+    die "Input file format error\n" if (($leftnum != $num) || ($rightnum != $num));
 
-    my $leftpos = -1; my $rightpos = -1;
-    my $leftrst = -1; my $rightrst = -1;
+    my $flag = 0;
+    $flag |= FL_LEFT_INVERSE if ($leftflag & 16);
+    $flag |= FL_RIGHT_INVERSE if ($rightflag & 16);
+    $flag |= FL_LEFT_ALIGN if ((!($leftflag & 4)) && ($leftqual >= 30));
+    $flag |= FL_RIGHT_ALIGN if ((!($rightflag & 4)) && ($rightqual >= 30));
+    $flag |= FL_INTRA_CHR if (($leftchr eq $rightchr) && ($leftchr ne "*") && ($leftchr ne "*"));
 
-    $flag = 0;
-    if ($left[0] != $num) {
-	$flag |= FL_LEFT_NOTFOUND;
-    } else { 
-	$leftpos = $left[3];
-	$leftrst = findinrst($leftpos);
-
-	my $leftpos2 = $leftpos;
-	if ($left[2] & 16) {
-	    $flag |= FL_LEFT_INVERSE;
-	}
-    }
-    if ($right[0] != $num) {
-	$flag |= FL_RIGHT_NOTFOUND;
-    } else {
-	$rightpos = $right[3];
-	$rightrst = findinrst($rightpos);
-
-	if ($right[2] & 16) {
-	    $flag |= FL_RIGHT_INVERSE;
-	}
-    }
-
-    my $distance = 0;
-    my $rstdist = 0;
-    if ($flag & (FL_LEFT_NOTFOUND | FL_RIGHT_NOTFOUND)) {
-	$distance = -1;
-	$rstdist = -1;
-    } else {
+    my $distance; my $rstdist;
+    if ($flag & FL_INTRA_CHR) {
 	if ($leftpos >= $rightpos) {
 	    $distance = $leftpos - $rightpos;
 	    $rstdist = $leftrst - $rightrst;
-	    if ($distance > $genomel / 2) {
-		$distance = $genomel - $distance;
-                $rstdist = $#start - $rstdist;
-	    } else {
-		$flag |= FL_INVERSE;
-	    }
 	} else {
 	    $distance = $rightpos - $leftpos;
 	    $rstdist = $rightrst - $leftrst;
-            if ($distance > $genomel / 2) {
-                $distance = $genomel - $distance;
-		$rstdist = $#start - $rstdist;
-		$flag |= FL_INVERSE;
-	    }
 	}
+    } else {
+	$distance = "*";
+	$rstdist = "*";
     }
-
-    printf CLASSIFIC "%d\t%.5b\t%d\t%d\t%d\t%d\t%d\t%d\n", $num, $flag, $leftpos, $rightpos, $distance, $leftrst, $rightrst, $rstdist;
+    print CLASSIFIC $num, "\t", $flag, "\t"
+	, $leftchr, "\t", $leftpos, "\t", $leftrst, "\t"
+	, $rightchr, "\t", $rightpos, "\t", $rightrst, "\t"
+	, $distance, "\t", $rstdist, "\n";
 
     last if (eof(LEFTMAP) && eof(RIGHTMAP));
 }
