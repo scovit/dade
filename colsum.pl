0d934f54a9f93018a9f0e07fff92df8abb3c7d24
Vittore F. Scolari <vittore.scolari@pasteur.fr>
2015-02-20 13:53:02 +0100

    Fixed colsum small bugs

--- colsum.pl
+++ colsum.pl
@@ -4,11 +4,11 @@ use warnings;
 
 # Takes as input matrix, output as dense matrix
 
-if ($#ARGV != 1) {
+if ($#ARGV != 0) {
 	print "usage: ./colsum.pl matrix\n";
 	exit;
 };
-my ($matrixfn, $densematrix) = @ARGV;
+my ($matrixfn) = @ARGV;
 
 # open input files
 if ($matrixfn eq '-') {
@@ -28,14 +28,14 @@ while(<MATRIX>) {
     my @input = split("\t");
     my $title = shift(@input);
 
-    for $j (0..$#input) {
+    for my $j (0..$#input) {
 	$output[$j] = 0 unless exists $output[$j];
 	$output[$j] += $input[$j];
     }
 }
 close(MATRIX);
 
-for $i (0..$#output) {
+for my $i (0..$#output) {
     print $i, "\t", $output[$i], "\n";
 }
 
