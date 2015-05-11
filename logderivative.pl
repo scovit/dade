9736f1518e6554310555f3730311b1a21ed842b4
Vittore F. Scolari <vittore.scolari@pasteur.fr>
2015-05-11 14:27:28 +0200

    Corrected some problem in logderivative

--- logderivative.pl
+++ logderivative.pl
@@ -23,9 +23,11 @@ if ($vectorfn eq '-') {
 my @xax;
 my @histogram;
 my @variance;
+my $novariance = 0;
 while(<VECTOR>) {
     chomp;
     my ($x,$y,$z) = split("\t");
+    $novariance++ unless defined($z);
     push @xax, $x;
     push @histogram, $y;
     push @variance, $z;
@@ -42,6 +44,9 @@ die "Binning does not looks like logarithmic ($m vs $v)" if
     sqrt($v) > 0.1 * $m;
 my $steps = int($m * 100 + 0.5) / 100;
 
+# guess start point
+
+
 # get maximun nonzero value
 my $maxind = $#xax;
 for ( ; $maxind >= 0; $maxind--) {
@@ -50,11 +55,12 @@ for ( ; $maxind >= 0; $maxind--) {
 $maxind++ if $maxind != 0;
 
 # get last zero before maxind 
-my $minind;
+my $minind = -1;
 for (my $k = 0; $k < $maxind; $k++) {
     $minind = $k unless $histogram[$k];
 }
 $minind++;
+
 
 # calculate log derivative
 my @logderivative;
@@ -62,12 +68,15 @@ my @logdervariance;
 my $old = $histogram[$minind];
 my $oldvar = $variance[$minind];
 for (my $k = $minind + 1; $k < $maxind; $k++) {
-    $logderivative[$k] = (log($histogram[$k])-log($old)) 
-	/ (log($steps**$k) - log($steps**($k-1)));
+    $logderivative[$k] = (log($histogram[$k])-log($old))
+	/ log($steps);
     # Crappy method that ignore non-linearities
-    $logdervariance[$k] = 1.0 / ((log($steps**$k) - log($steps**($k-1)))**2) * (
-	1.0/($histogram[$k]**2) * $variance[$k] +
-	1.0/($old**2)) * $oldvar;
+    unless ($novariance) {
+	$logdervariance[$k] = 1.0 /
+	    ((log($steps**$k) - log($steps**($k-1)))**2) * (
+		1.0/($histogram[$k]**2) * $variance[$k] +
+		1.0/($old**2)) * $oldvar;
+    }
     $old = $histogram[$k]; $oldvar = $variance[$k];
 }
 
