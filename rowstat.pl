7e07b092be6c1581610b5ab118eff41c3955a595
Vittore F. Scolari <vittore.scolari@pasteur.fr>
2015-05-26 16:50:20 +0200

    Modify rowstat arguments

--- rowstat.pl
+++ rowstat.pl
@@ -5,25 +5,15 @@ use warnings;
 # Takes as input matrix, output a vector
 # Basically a oneliner, but since we have colsum ....
 
-if ($#ARGV != 0) {
-    print STDERR "usage: ./rowsum.pl matrix\n";
+if ($#ARGV != -1) {
+    print STDERR "usage: ./rowsum.pl < matrix\n";
     exit;
-}
-my $matrixfn = shift @ARGV;
-
-# open input files
-if ($matrixfn eq '-') {
-    *MATRIX = *STDIN;
-} elsif ($matrixfn =~ /\.gz$/) {
-    open(MATRIX, "gzip -d -c $matrixfn |");
-} else {
-    open(MATRIX, "< $matrixfn");
 }
 
 # output, stdout
-my $header = <MATRIX>;
+my $header = <>;
 print "\"ROWSTAT\"\n";
-while(<MATRIX>) {
+while(<>) {
     chomp;
     my @input = split("\t");
     my $title = shift(@input);
@@ -44,6 +34,5 @@ while(<MATRIX>) {
     
     print join("\t", "\"$title\"", $len, $sum, $mean, $variance), "\n";
 }
-close(MATRIX);
 
 0;
