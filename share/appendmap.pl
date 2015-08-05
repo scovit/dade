6dff721b7c6eb863c4cfd58ce4588d830eeea15e
Vittore F. Scolari <vittore.scolari@pasteur.fr>
2015-08-05 12:50:40 +0200

    appendmap now pass the ambiguous reads by default

--- share/appendmap.pl
+++ share/appendmap.pl
@@ -9,6 +9,27 @@ BEGIN {
 
 our %chrlength;
 
+sub findshifts ($) {
+    my $CIGAR = shift;
+    my @CINFO = map { [ m/([0-9]+)([MIDNSHPX\=])/ ] } (
+	$CIGAR =~ m/([0-9]+[MIDNSHPX\=])/g );
+    my $LSHIFT = 0;
+    my $RSHIFT = 0;
+
+    for my $inf (@CINFO) {
+	my ($value, $operator) = @$inf;
+	last if ($operator eq 'M');
+	$LSHIFT += $value if ($operator eq 'N');
+	
+    }
+    for my $inf (reverse @CINFO) {
+	last if ($inf->[1] eq 'M');
+    }
+
+    return ($LSHIFT, $RSHIFT);
+}
+
+
 sub appendmap {
     if ($#_ != 5 ) {
 	print "appendmap needs 6 arguments: mapfile, samfile, length, step, readlength, minq\n";
@@ -24,19 +45,20 @@ sub appendmap {
         {
             print $_,"\n";
         } else {
-            my ($NAME, $FLAG, $CHR, $POS, $MAPQ) = split("\t");
+            my ($NAME, $FLAG, $CHR, $POS, $MAPQ, $CIGAR) = split("\t");
             while ($length->[$index] == 0) {
                 $index++;
             }
+	    my $isambig = scalar ($_ =~ /XS:i:[0-9-]/);
 
-            if (($MAPQ < $minq) || ($_ =~ /XS:i:[0-9-]/)) {
+            if ($MAPQ < $minq) {
 # Unmapped
 		my $lengthvar = $length->[$index];
 
 		# if it's last iteration, just print it.
 		if ($lengthvar == $rlength) {
 		    $POS += ($length->[$index] - 1) if ($FLAG & 16);
-		    $FLAG |= 4096 if ($_ =~ /XS:i:[0-9-]/);
+		    $FLAG |= 4096 if $isambig;
 		    my $RST = findinrst($POS, $CHR);
 		    if (!defined $RST) {
 			warn "Could not find RST, setting last, out: $_";
@@ -57,7 +79,9 @@ sub appendmap {
 		}
             } else {
 # Mapped
+                $FLAG |= 4096 if $isambig;
 		$POS += ($length->[$index] - 1) if ($FLAG & 16);
+
 		my $RST = findinrst($POS, $CHR);
 		if (!defined $RST) {
 		    warn "Could not find RST, setting last, out: $_";
