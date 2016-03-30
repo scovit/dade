846d2eda25be6cae8db314528f87235bc51ec395
Vittore F. Scolari <vittore.scolari@pasteur.fr>
2016-03-30 12:05:47 +0200

    Simplify pick.pl

--- pick.pl
+++ pick.pl
@@ -1,7 +1,6 @@
 #!/bin/env perl
 
 use strict;
-use IPC::Open3;
 
 BEGIN {
     use FindBin '$Bin';
@@ -20,36 +19,26 @@ my $compiled = eval
     'sub { local %_ = %{ shift @_; }; '. $blockstring .' }';
 die $@ unless($compiled);
 
-my $pid = open3("<&STDIN", my $selectionIPC, ">&STDERR", "-");
-unless ($pid) {
-    while (<STDIN>) {
-	my %in = parse_class;
-	if ($compiled->(\%in)) {
-	    print;
-	}
-    }
-    exit(0);
-}
-
 open(my $left , "<", $leftmapfn);
 open(my $right, "<", $rightmapfn);
 
-while (<$selectionIPC>) {
+while (<STDIN>) {
     my %in = parse_class;
 
-    # Find the sequences
-    my $leftl; my $rigtl;
-    while($leftl = <$left>) {
-	$rigtl = <$right>;
-	last if($. == $in{INDEX} * 4 + 2);
-    }
+    if ($compiled->(\%in)) {
+	# Find the sequences
+	my $leftl; my $rigtl;
+	while($leftl = <$left>) {
+	    $rigtl = <$right>;
+	    last if($. == $in{INDEX} * 4 + 2);
+	}
 
-    print join("\t", $in{INDEX}, $in{FLAG},
-	       $in{LEFTCHR}, $in{LEFTPOS}, $in{LEFTRST}, $leftl);
-    print join("\t", $in{INDEX}, $in{FLAG},
-	       $in{RIGHTCHR}, $in{RIGHTPOS}, $in{RIGHTRST}, $rigtl);
+	print join("\t", $in{INDEX}, $in{FLAG},
+		   $in{LEFTCHR}, $in{LEFTPOS}, $in{LEFTRST}, $leftl);
+	print join("\t", $in{INDEX}, $in{FLAG},
+		   $in{RIGHTCHR}, $in{RIGHTPOS}, $in{RIGHTRST}, $rigtl);
+    }
 }
 
-waitpid( $pid, 0 );
 close LEFT;
 close RIGT;
